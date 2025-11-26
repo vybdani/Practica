1fd690e2a546e4e8cc4259340f814d832995ddd0
vybdani <darochap@uamv.edu.ni>
2025-11-26 07:13:56 -0600

    Ejercicio 2

---
+++ Practica/Ejercicio2.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Practica
+{
+    partial class Ejercicio2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 29);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(300, 148);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(51, 197);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(231, 42);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar y Ordenar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "¿Qué número buscas?";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(182, 265);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(143, 22);
+            this.txtBuscar.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(51, 311);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(231, 42);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Búsqueda Binaria";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(25, 381);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(300, 148);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Ejercicio2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 558);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Ejercicio2";
+            this.Text = "Ejercicio2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
---
+++ Practica/Ejercicio2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Ejercicio2 : Form
+    {
+        int[] lista = new int[30];
+        private ListBox lstNumeros;
+
+        public Ejercicio2()
+        {
+            InitializeComponent();
+            lstNumeros = listBox1;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            lstNumeros.Items.Clear();
+
+            for (int i = 0; i < 30; i++)
+            {
+                lista[i] = r.Next(1, 101);
+            }
+
+            Array.Sort(lista);
+
+            for (int i = 0; i < 30; i++)
+            {
+                lstNumeros.Items.Add(lista[i]);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int buscado = int.Parse(txtBuscar.Text);
+            int inicio = 0;
+            int fin = 29;
+            bool encontrado = false;
+
+            while (inicio <= fin)
+            {
+                int mitad = (inicio + fin) / 2;
+
+                if (lista[mitad] == buscado)
+                {
+                    MessageBox.Show("Encontrado en la posición: " + mitad);
+                    encontrado = true;
+                    break;
+                }
+
+                if (buscado < lista[mitad])
+                {
+                    fin = mitad - 1;
+                }
+                else
+                {
+                    inicio = mitad + 1;
+                }
+            }
+
+            if (encontrado == false)
+            {
+                MessageBox.Show("El número no está en la lista");
+            }
+        }
+    }
+}
