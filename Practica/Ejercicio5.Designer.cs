424b89ec5a3d8dcf6bbb86503c9b8d800c29d075
vybdani <darochap@uamv.edu.ni>
2025-11-26 17:56:39 -0600

    Ejercicio5

---
+++ Practica/Ejercicio5.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Practica
+{
+    partial class Ejercicio5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumeros = new System.Windows.Forms.ListBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblIteraciones = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumeros
+            // 
+            this.lstNumeros.FormattingEnabled = true;
+            this.lstNumeros.ItemHeight = 16;
+            this.lstNumeros.Location = new System.Drawing.Point(21, 24);
+            this.lstNumeros.Name = "lstNumeros";
+            this.lstNumeros.Size = new System.Drawing.Size(304, 84);
+            this.lstNumeros.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(21, 123);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(148, 48);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar Lista";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(177, 123);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(148, 48);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Buscar Máx y Mín";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Máximo:";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(112, 198);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(16, 16);
+            this.lblMax.TabIndex = 4;
+            this.lblMax.Text = "...";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(112, 233);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(16, 16);
+            this.lblMin.TabIndex = 6;
+            this.lblMin.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mínimo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(127, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "...";
+            // 
+            // lblIteraciones
+            // 
+            this.lblIteraciones.AutoSize = true;
+            this.lblIteraciones.Location = new System.Drawing.Point(45, 268);
+            this.lblIteraciones.Name = "lblIteraciones";
+            this.lblIteraciones.Size = new System.Drawing.Size(76, 16);
+            this.lblIteraciones.TabIndex = 7;
+            this.lblIteraciones.Text = "Iteraciones:";
+            // 
+            // Ejercicio5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 336);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblIteraciones);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.lstNumeros);
+            this.Name = "Ejercicio5";
+            this.Text = "Ejercicio5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumeros;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblIteraciones;
+    }
+}
---
+++ Practica/Ejercicio5.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Ejercicio5 : Form
+    {
+        List<int> lista = new List<int>();
+        public Ejercicio5()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            lista.Clear();
+            lstNumeros.Items.Clear();
+
+            for (int i = 0; i < 20; i++)
+            {
+                int numero = rnd.Next(1, 101);
+                lista.Add(numero);
+                lstNumeros.Items.Add(numero);
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (lista.Count == 0)
+            {
+                MessageBox.Show("Primero genera la lista");
+                return;
+            }
+
+            int mayor = lista[0];
+            int menor = lista[0];
+            int contador = 0;
+
+            foreach (int numero in lista)
+            {
+                contador++;
+
+                if (numero > mayor)
+                {
+                    mayor = numero;
+                }
+
+                if (numero < menor)
+                {
+                    menor = numero;
+                }
+            }
+
+            lblMax.Text = mayor.ToString();
+            lblMin.Text = menor.ToString();
+            lblIteraciones.Text = contador.ToString();
+        }
+    }
+}
