c492e7613c2a9b4d58a06341e5cb19a9eff2cddf
vybdani <darochap@uamv.edu.ni>
2025-11-26 17:29:07 -0600

    Ejercicio4

---
+++ Practica/Ejercicio4.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Practica
+{
+    partial class Ejercicio4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdNuevo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombreNuevo = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lstEstudiantes = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIdBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscarId = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscarNombre = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Nuevo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIdNuevo
+            // 
+            this.txtIdNuevo.Location = new System.Drawing.Point(104, 30);
+            this.txtIdNuevo.Name = "txtIdNuevo";
+            this.txtIdNuevo.Size = new System.Drawing.Size(196, 22);
+            this.txtIdNuevo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre Nuevo:";
+            // 
+            // txtNombreNuevo
+            // 
+            this.txtNombreNuevo.Location = new System.Drawing.Point(131, 86);
+            this.txtNombreNuevo.Name = "txtNombreNuevo";
+            this.txtNombreNuevo.Size = new System.Drawing.Size(169, 22);
+            this.txtNombreNuevo.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(82, 135);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(130, 36);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lstEstudiantes
+            // 
+            this.lstEstudiantes.FormattingEnabled = true;
+            this.lstEstudiantes.ItemHeight = 16;
+            this.lstEstudiantes.Location = new System.Drawing.Point(26, 200);
+            this.lstEstudiantes.Name = "lstEstudiantes";
+            this.lstEstudiantes.Size = new System.Drawing.Size(274, 84);
+            this.lstEstudiantes.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Buscar ID:";
+            // 
+            // txtIdBuscar
+            // 
+            this.txtIdBuscar.Location = new System.Drawing.Point(434, 57);
+            this.txtIdBuscar.Name = "txtIdBuscar";
+            this.txtIdBuscar.Size = new System.Drawing.Size(170, 22);
+            this.txtIdBuscar.TabIndex = 7;
+            // 
+            // btnBuscarId
+            // 
+            this.btnBuscarId.Location = new System.Drawing.Point(416, 99);
+            this.btnBuscarId.Name = "btnBuscarId";
+            this.btnBuscarId.Size = new System.Drawing.Size(130, 36);
+            this.btnBuscarId.TabIndex = 8;
+            this.btnBuscarId.Text = "Buscar ID";
+            this.btnBuscarId.UseVisualStyleBackColor = true;
+            this.btnBuscarId.Click += new System.EventHandler(this.btnBuscarId_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(363, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Buscar Nombre:";
+            // 
+            // txtNombreBuscar
+            // 
+            this.txtNombreBuscar.Location = new System.Drawing.Point(473, 162);
+            this.txtNombreBuscar.Name = "txtNombreBuscar";
+            this.txtNombreBuscar.Size = new System.Drawing.Size(131, 22);
+            this.txtNombreBuscar.TabIndex = 10;
+            // 
+            // btnBuscarNombre
+            // 
+            this.btnBuscarNombre.Location = new System.Drawing.Point(416, 207);
+            this.btnBuscarNombre.Name = "btnBuscarNombre";
+            this.btnBuscarNombre.Size = new System.Drawing.Size(130, 36);
+            this.btnBuscarNombre.TabIndex = 11;
+            this.btnBuscarNombre.Text = "Buscar Nombre";
+            this.btnBuscarNombre.UseVisualStyleBackColor = true;
+            this.btnBuscarNombre.Click += new System.EventHandler(this.btnBuscarNombre_Click);
+            // 
+            // Ejercicio4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 322);
+            this.Controls.Add(this.btnBuscarNombre);
+            this.Controls.Add(this.txtNombreBuscar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBuscarId);
+            this.Controls.Add(this.txtIdBuscar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstEstudiantes);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombreNuevo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdNuevo);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio4";
+            this.Text = "Ejercicio4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdNuevo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombreNuevo;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox lstEstudiantes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIdBuscar;
+        private System.Windows.Forms.Button btnBuscarId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombreBuscar;
+        private System.Windows.Forms.Button btnBuscarNombre;
+    }
+}
---
+++ Practica/Ejercicio4.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Ejercicio4 : Form
+    {
+        List<Estudiante> lista = new List<Estudiante>();
+        public class Estudiante
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        public Ejercicio4()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtIdNuevo.Text == "" || txtNombreNuevo.Text == "") return;
+
+            Estudiante nuevo = new Estudiante();
+            nuevo.Id = int.Parse(txtIdNuevo.Text);
+            nuevo.Nombre = txtNombreNuevo.Text;
+
+            lista.Add(nuevo);
+            lstEstudiantes.Items.Add(nuevo.Id + " - " + nuevo.Nombre);
+
+            txtIdNuevo.Clear();
+            txtNombreNuevo.Clear();
+            txtIdNuevo.Focus();
+        }
+
+        private void btnBuscarId_Click(object sender, EventArgs e)
+        {
+            if (txtIdBuscar.Text == "") return;
+
+            int idBuscado = int.Parse(txtIdBuscar.Text);
+            bool encontrado = false;
+
+            foreach (Estudiante est in lista)
+            {
+                if (est.Id == idBuscado)
+                {
+                    MessageBox.Show("Encontrado: " + est.Nombre);
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (encontrado == false)
+            {
+                MessageBox.Show("ID no encontrado");
+            }
+        }
+
+        private void btnBuscarNombre_Click(object sender, EventArgs e)
+        {
+            if (txtNombreBuscar.Text == "") return;
+
+            string buscado = txtNombreBuscar.Text;
+
+            lista.Sort((x, y) => x.Nombre.CompareTo(y.Nombre));
+
+            lstEstudiantes.Items.Clear();
+            foreach (Estudiante est in lista)
+            {
+                lstEstudiantes.Items.Add(est.Id + " - " + est.Nombre);
+            }
+
+            int inicio = 0;
+            int fin = lista.Count - 1;
+            bool encontrado = false;
+
+            while (inicio <= fin)
+            {
+                int mitad = (inicio + fin) / 2;
+                string nombreCentro = lista[mitad].Nombre;
+                int comparacion = nombreCentro.CompareTo(buscado);
+
+                if (comparacion == 0)
+                {
+                    MessageBox.Show("Encontrado ID: " + lista[mitad].Id);
+                    encontrado = true;
+                    break;
+                }
+
+                if (comparacion < 0)
+                {
+                    inicio = mitad + 1;
+                }
+                else
+                {
+                    fin = mitad - 1;
+                }
+            }
+
+            if (encontrado == false)
+            {
+                MessageBox.Show("Nombre no encontrado");
+            }
+        }
+    }
+}
