0131d69066229b7fa53cf7bea0012230b42937d7
vybdani <darochap@uamv.edu.ni>
2025-11-27 14:11:55 -0600

    Codigo semi completado

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,55 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvLibros = new System.Windows.Forms.DataGridView();
+            this.dgvBiblioteca = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.txtTitulo = new System.Windows.Forms.TextBox();
             this.btnBuscarTitulo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.btnBuscarAutor = new System.Windows.Forms.Button();
             this.txtAutor = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscarDesc = new System.Windows.Forms.Button();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).BeginInit();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnExtremos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBiblioteca)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dgvLibros
+            // dgvBiblioteca
             // 
-            this.dgvLibros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLibros.Location = new System.Drawing.Point(12, 12);
-            this.dgvLibros.Name = "dgvLibros";
-            this.dgvLibros.RowHeadersWidth = 51;
-            this.dgvLibros.RowTemplate.Height = 24;
-            this.dgvLibros.Size = new System.Drawing.Size(839, 125);
-            this.dgvLibros.TabIndex = 0;
+            this.dgvBiblioteca.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBiblioteca.Location = new System.Drawing.Point(12, 12);
+            this.dgvBiblioteca.Name = "dgvBiblioteca";
+            this.dgvBiblioteca.RowHeadersWidth = 51;
+            this.dgvBiblioteca.RowTemplate.Height = 24;
+            this.dgvBiblioteca.Size = new System.Drawing.Size(469, 125);
+            this.dgvBiblioteca.TabIndex = 0;
+            this.dgvBiblioteca.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBiblioteca_CellContentClick);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnBuscarTitulo);
             this.groupBox1.Controls.Add(this.txtTitulo);
+            this.groupBox1.Controls.Add(this.btnBuscarTitulo);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 152);
+            this.groupBox1.Location = new System.Drawing.Point(37, 152);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(212, 138);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Búsqueda por Título";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Título:";
             // 
             // txtTitulo
             // 
@@ -87,13 +82,22 @@
             // 
             // btnBuscarTitulo
             // 
-            this.btnBuscarTitulo.Location = new System.Drawing.Point(45, 75);
+            this.btnBuscarTitulo.Location = new System.Drawing.Point(58, 75);
             this.btnBuscarTitulo.Name = "btnBuscarTitulo";
             this.btnBuscarTitulo.Size = new System.Drawing.Size(107, 32);
             this.btnBuscarTitulo.TabIndex = 2;
             this.btnBuscarTitulo.Text = "Buscar";
             this.btnBuscarTitulo.UseVisualStyleBackColor = true;
             this.btnBuscarTitulo.Click += new System.EventHandler(this.btnBuscarTitulo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Título:";
             // 
             // groupBox2
             // 
@@ -115,6 +119,7 @@
             this.btnBuscarAutor.TabIndex = 2;
             this.btnBuscarAutor.Text = "Buscar";
             this.btnBuscarAutor.UseVisualStyleBackColor = true;
+            this.btnBuscarAutor.Click += new System.EventHandler(this.btnBuscarAutor_Click);
             // 
             // txtAutor
             // 
@@ -134,31 +139,32 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.btnBuscarDesc);
+            this.groupBox3.Controls.Add(this.txtDescripcion);
             this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Location = new System.Drawing.Point(530, 152);
+            this.groupBox3.Location = new System.Drawing.Point(37, 327);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(212, 138);
             this.groupBox3.TabIndex = 3;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Buscar en Descripción";
             // 
-            // button1
+            // btnBuscarDesc
             // 
-            this.button1.Location = new System.Drawing.Point(45, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Buscar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBuscarDesc.Location = new System.Drawing.Point(45, 75);
+            this.btnBuscarDesc.Name = "btnBuscarDesc";
+            this.btnBuscarDesc.Size = new System.Drawing.Size(107, 32);
+            this.btnBuscarDesc.TabIndex = 2;
+            this.btnBuscarDesc.Text = "Buscar";
+            this.btnBuscarDesc.UseVisualStyleBackColor = true;
+            this.btnBuscarDesc.Click += new System.EventHandler(this.btnBuscarDesc_Click_1);
             // 
-            // textBox1
+            // txtDescripcion
             // 
-            this.textBox1.Location = new System.Drawing.Point(108, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(87, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtDescripcion.Location = new System.Drawing.Point(108, 36);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(87, 22);
+            this.txtDescripcion.TabIndex = 1;
             // 
             // label3
             // 
@@ -169,31 +175,53 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Palabra clave:";
             // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnExtremos);
+            this.groupBox4.Location = new System.Drawing.Point(269, 327);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(212, 138);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Reportes";
+            // 
+            // btnExtremos
+            // 
+            this.btnExtremos.Location = new System.Drawing.Point(20, 36);
+            this.btnExtremos.Name = "btnExtremos";
+            this.btnExtremos.Size = new System.Drawing.Size(163, 84);
+            this.btnExtremos.TabIndex = 2;
+            this.btnExtremos.Text = "Ver Antiguo-Nuevo";
+            this.btnExtremos.UseVisualStyleBackColor = true;
+            this.btnExtremos.Click += new System.EventHandler(this.btnExtremos_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.ClientSize = new System.Drawing.Size(544, 528);
             this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dgvLibros);
+            this.Controls.Add(this.dgvBiblioteca);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvLibros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBiblioteca)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvLibros;
+        private System.Windows.Forms.DataGridView dgvBiblioteca;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnBuscarTitulo;
         private System.Windows.Forms.TextBox txtTitulo;
@@ -203,9 +231,11 @@
         private System.Windows.Forms.TextBox txtAutor;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBuscarDesc;
+        private System.Windows.Forms.TextBox txtDescripcion;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnExtremos;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,14 +10,168 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+
     public partial class Form1 : Form
     {
+        List<Libro> biblioteca = new List<Libro>();
         public Form1()
         {
             InitializeComponent();
         }
 
         private void btnBuscarTitulo_Click(object sender, EventArgs e)
+        {
+            string tituloBuscado = txtTitulo.Text.ToLower();
+            bool encontrado = false;
+            foreach (var libro in biblioteca)
+            {
+                if (libro.Titulo.ToLower().Contains(tituloBuscado))
+                {
+                    MessageBox.Show($"Libro encontrado:\nTítulo: {libro.Titulo}\nAutor: {libro.Autor}\nAño: {libro.Anio}\nDescripción: {libro.Descripcion}");
+                    encontrado = true;
+                    break;
+                }
+            }
+            if (encontrado == false)
+            {
+                MessageBox.Show("Libro no encontrado.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            biblioteca.Add(new Libro { Titulo = "Cien anios de soledad", Autor = "Gabriel Garcia", Anio = 1967, Descripcion = "Novela sobre la familia Buendia" });
+            biblioteca.Add(new Libro { Titulo = "Don Quijote", Autor = "Miguel de Cervantes", Anio = 1605, Descripcion = "Un hidalgo que se cree caballero" });
+            biblioteca.Add(new Libro { Titulo = "Harry Potter", Autor = "JK Rowling", Anio = 1997, Descripcion = "Un niño mago va a una escuela" });
+            biblioteca.Add(new Libro { Titulo = "El Principito", Autor = "Antoine Saint", Anio = 1943, Descripcion = "Un aviador conoce a un niño" });
+            biblioteca.Add(new Libro { Titulo = "1984", Autor = "George Orwell", Anio = 1949, Descripcion = "Historia sobre el control total" });
+
+            ActualizarTabla();  
+        }
+
+        private void ActualizarTabla()
+        {
+            dgvBiblioteca.Rows.Clear();
+            foreach (var libro in biblioteca)
+            {
+                dgvBiblioteca.Rows.Add(libro.Titulo, libro.Autor, libro.Anio, libro.Descripcion);
+            }
+
+        }
+
+        public class Libro
+        {
+            public string Titulo { get; set; }
+            public string Autor { get; set; }
+            public int Anio { get; set; }
+            public string Descripcion { get; set; }
+        }
+
+        private void btnBuscarAutor_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < biblioteca.Count - 1; i++)
+            {
+                for (int j = 0; j < biblioteca.Count - i - 1; j++)
+                {
+                    if (string.Compare(biblioteca[j].Autor, biblioteca[j + 1].Autor) > 0)
+                    {
+                        Libro temp = biblioteca[j];
+                        biblioteca[j] = biblioteca[j + 1];
+                        biblioteca[j + 1] = temp;
+                    }
+                }
+            }
+
+            ActualizarTabla();
+
+            string buscado = txtAutor.Text;
+            int inicio = 0;
+            int fin = biblioteca.Count - 1;
+            bool encontrado = false;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+                int comparacion = string.Compare(biblioteca[medio].Autor, buscado);
+
+                if (comparacion == 0)
+                {
+                    MessageBox.Show("¡ENCONTRADO!\nLibro: " + biblioteca[medio].Titulo);
+                    encontrado = true;
+                    break;
+                }
+
+                if (comparacion < 0) inicio = medio + 1;
+                else fin = medio - 1;
+            }
+
+            if (encontrado == false) MessageBox.Show("Autor no encontrado");
+        }
+
+        private void btnExtremos_Click(object sender, EventArgs e)
+        {
+            if (biblioteca.Count == 0) return;
+
+            Libro masViejo = biblioteca[0];
+            Libro masNuevo = biblioteca[0];
+
+            foreach (Libro libro in biblioteca)
+            {
+                if (libro.Anio < masViejo.Anio) masViejo = libro;
+                if (libro.Anio > masNuevo.Anio) masNuevo = libro;
+            }
+
+            MessageBox.Show("Más Viejo: " + masViejo.Titulo + " (" + masViejo.Anio + ")\n" +
+                            "Más Nuevo: " + masNuevo.Titulo + " (" + masNuevo.Anio + ")");
+        }
+
+        private void btnBuscarDesc_Click(object sender, EventArgs e)
+        {
+            string palabra = txtDescripcion.Text.ToLower();
+            string resultados = "";
+
+            foreach (Libro libro in biblioteca)
+            {
+                string texto = libro.Descripcion.ToLower();
+                bool contiene = false;
+
+                // Algoritmo manual para buscar texto dentro de texto
+                for (int i = 0; i <= texto.Length - palabra.Length; i++)
+                {
+                    bool coincide = true;
+                    for (int j = 0; j < palabra.Length; j++)
+                    {
+                        if (texto[i + j] != palabra[j])
+                        {
+                            coincide = false;
+                            break;
+                        }
+                    }
+                    if (coincide)
+                    {
+                        contiene = true;
+                        break;
+                    }
+                }
+
+                if (contiene) resultados += "- " + libro.Titulo + "\n";
+            }
+
+            if (resultados != "") MessageBox.Show("Encontrado en:\n" + resultados);
+            else MessageBox.Show("Ninguna descripción tiene esa palabra");
+        }
+
+        private void dgvBiblioteca_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnExtremos_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscarDesc_Click_1(object sender, EventArgs e)
         {
 
         }
