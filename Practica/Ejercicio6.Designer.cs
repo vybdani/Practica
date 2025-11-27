9829006a29971c1da2835dce22d93203851dbe39
vybdani <darochap@uamv.edu.ni>
2025-11-26 18:17:24 -0600

    Ejercicio6

---
+++ Practica/Ejercicio6.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Practica
+{
+    partial class Ejercicio6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMatriz = new System.Windows.Forms.RichTextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbMatriz
+            // 
+            this.rtbMatriz.Location = new System.Drawing.Point(21, 24);
+            this.rtbMatriz.Name = "rtbMatriz";
+            this.rtbMatriz.ReadOnly = true;
+            this.rtbMatriz.Size = new System.Drawing.Size(275, 126);
+            this.rtbMatriz.TabIndex = 0;
+            this.rtbMatriz.Text = "";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(76, 175);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(161, 43);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar Matriz";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Buscar número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(146, 243);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(150, 22);
+            this.txtNumero.TabIndex = 3;
+            this.txtNumero.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(76, 289);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(161, 43);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar en Matriz";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Ejercicio6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 365);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.rtbMatriz);
+            this.Name = "Ejercicio6";
+            this.Text = "Ejercicio6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMatriz;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
---
+++ Practica/Ejercicio6.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Ejercicio6 : Form
+    {
+        int[,] matriz = new int[10, 10];
+        public Ejercicio6()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            rtbMatriz.Clear();
+            string contenido = "";
+
+            for (int f = 0; f < 10; f++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    matriz[f, c] = rnd.Next(1, 100);
+                    contenido += matriz[f, c] + "\t";
+                }
+                contenido += "\n";
+            }
+
+            rtbMatriz.Text = contenido;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (txtNumero.Text == "") return;
+
+            int buscado = int.Parse(txtNumero.Text);
+            bool encontrado = false;
+            string posiciones = "";
+
+            for (int f = 0; f < 10; f++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    if (matriz[f, c] == buscado)
+                    {
+                        posiciones += "Fila " + f + ", Columna " + c + "\n";
+                        encontrado = true;
+                    }
+                }
+            }
+
+            if (encontrado)
+            {
+                MessageBox.Show("Encontrado en:\n" + posiciones);
+            }
+            else
+            {
+                MessageBox.Show("El número no está en la matriz.");
+            }
+        }
+    }
+}
